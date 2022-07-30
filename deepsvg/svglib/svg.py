7a9be11cf841a807ffa95a41fa74782421d59205
RJ Adriaansen <adriaansen@eshcc.eur.nl>
2022-07-30 08:40:28 +0200

    Fix viewbox
    https://github.com/alexandre01/deepsvg/issues/17

--- deepsvg/svglib/svg.py
+++ deepsvg/svglib/svg.py
@@ -124,6 +124,8 @@ class SVG:
         viewbox_list = None
         if svg_root.hasAttribute('viewBox'):
             viewbox = list(map(float, svg_root.getAttribute("viewBox").split(" ")))
+        elif svg_root.hasAttribute('viewbox'):
+            viewbox = list(map(float, svg_root.getAttribute("viewbox").split(" ")))
         elif svg_root.hasAttribute('width') and svg_root.hasAttribute('height'):
             viewbox_list = [0, 0, float(svg_root.getAttribute("width")), float(svg_root.getAttribute("height"))]
         else:
